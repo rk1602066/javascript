b0514ab51a9c0d1ffd696ef67498cec96929606b
rk1602066 <66769695+rk1602066@users.noreply.github.com>
2020-08-26 00:26:04 -0700

    Create 002Variable.js

---
+++ 002Variable.js
@@ -0,0 +1,105 @@
+//asign variable name a and put value 9 within a;
+var a = 9
+//asign value and variable to another variable and some operation
+// Only change code below this line
+var a=5;
+var b=10;
+var c="I am a";
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+/*
+In JavaScript all variables and function names are case sensitive. This means that capitalization matters.
+
+MYVAR is not the same as MyVar nor myvar
+*/
+/*
+Write variable names in JavaScript in camelCase. In camelCase, multi-word variable
+names have the first word in lowercase and the first letter of each subsequent word is capitalized.
+*/
+
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar= 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+/*
+Add Two Numbers with JavaScript
+Number is a data type in JavaScript which represents
+*/
+myVar = 5 + 10; // assigned 15
+
+//Subtract One Number from Another with JavaScript
+myVar = 12 - 6;
+
+//Multiply Two Numbers with JavaScript
+var product = 8 * 10;
+
+//Divide One Number by Another with JavaScript
+var quotient = 66 / 33;
+
+//Finding a Remainder in JavaScript
+var remainder = 11%3;
+
+
+//Increment a Number with JavaScript
+myVar++;  //now myvar will 88
+
+//Decrement a Number with JavaScript
+var myVar = 11;
+myVar--;  //now myVar in equal to 10;
+
+//Create Decimal Numbers with JavaScript
+var ourDecimal = 5.7;
+
+// Only change code below this line
+var myDecimal=5.7
+/*Not all real numbers can accurately be represented in floating point. This can lead to rounding errors*/
+
+//product ,quotien of decimal numbers
+var product = 2.0 * 5.0;
+var quotient = 4.4 / 2.0; 
+
+
+
+//Compound Assignment With Augmented Addition
+var a = 3;
+var b = 17;
+var c = 12;
+a += 12;
+b += 9;
+c += 7;
+
+
+//Compound Assignment With Augmented Subtraction
+var a = 11;
+var b = 9;
+var c = 3;
+a -= 6;
+b -= 15;
+c -= 1;
+
+//Compound Assignment With Augmented Multiplication
+var a = 5;
+var b = 12;
+var c = 4.6;
+a *= 5;
+b *= 3 ;
+c *= 10;
+
+//Compound Assignment With Augmented Division
+ var a = 48;
+var b = 108;
+var c = 33;
+a /= 12;
+b /= 4;
+c /= 11;
+
